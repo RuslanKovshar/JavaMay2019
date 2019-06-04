1c6ca5dbd0f2558be9adef2093191a4791c19a06
RuslanKovshar <ruslan.kovshar@gmail.com>
2019-06-04 10:41:03 +0300

    added comments

--- Arrays/src/Model.java
+++ Arrays/src/Model.java
@@ -2,6 +2,14 @@ public class Model {
 
     private int[] arr = {0,1,2,3,4};
 
+    /*
+    * дан масив {0,1,2,3,4}
+    * вывод в виде
+    * 0 0 0 0 0
+    * 1 1 1 1
+    * 2 2 2
+    * 3 3
+    * 4 */
     public void first(){
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length ; j++) {
@@ -11,6 +19,14 @@ public class Model {
         }
     }
 
+    /*
+     * дан масив {0,1,2,3,4}
+     * вывод в виде
+     * 0 1 2 3 4
+     * 0 1 2 3
+     * 0 1 2
+     * 0 1
+     * 0 */
     public void second(){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i ; j++) {
@@ -20,6 +36,15 @@ public class Model {
         }
     }
 
+
+    /*
+     * дан масив {0,1,2,3,4}
+     * вывод в виде
+     * 0 1 2 3 4
+     * 1 2 3 4
+     * 2 3 4
+     * 3 4
+     * 4 */
     public void third(){
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length ; j++) {
