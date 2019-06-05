27969e888bc9e1f93ddfc9533bb28c7bf064a9cf
RuslanKovshar <ruslan.kovshar@gmail.com>
2019-06-05 22:58:33 +0300

    Deleted unnecesary files

--- Arrays/src/View.java
+++
@@ -1,9 +0,0 @@
-public class View {
-    public final static String FIRST_TASK = "First task!";
-    public final static String SECOND_TASK = "Second task!";
-    public final static String THIRD_TASK = "Third task!";
-
-    public void printMessage(String msg){
-        System.out.println(msg);
-    }
-}
