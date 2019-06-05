b78bf0455fa031296a8e04330a19be09dc57fabc
RuslanKovshar <ruslan.kovshar@gmail.com>
2019-06-05 17:05:37 +0300

    deleted old files

--- RegistrationForm/src/com/kovshar/controller/RegularExpressions.java
+++
@@ -1,7 +0,0 @@
-package com.kovshar.controller;
-
-public interface RegularExpressions {
-    String firstName = "^[A-Z]{1}[a-z]+$";
-    String secondName = "^[A-Z]{1}[a-z]+$";
-    String nickName = "^[A-Za-z0-9_-]+$";
-}
