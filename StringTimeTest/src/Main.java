476263534ab3862d0c91b7c235e760600aa4acec
RuslanKovshar <ruslan.kovshar@gmail.com>
2019-06-05 23:13:06 +0300

    Deletet old files

--- StringTimeTest/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        TimeTester tester = new TimeTester();
-        tester.getStringTime();
-        tester.getStringBuildTime();
-    }
-}
