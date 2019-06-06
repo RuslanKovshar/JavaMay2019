9b9aea8301e14b797e9f64559226c2527f2576e7
RuslanKovshar <ruslan.kovshar@gmail.com>
2019-06-06 20:19:33 +0300

    Changed .gitignore

--- RegistrationForm/src/com/kovshar/controller/Controller.java
+++ RegistrationForm/src/com/kovshar/controller/Controller.java
@@ -5,11 +5,11 @@ import com.kovshar.view.View;
 
 import java.util.Scanner;
 
-public class Controller implements RegularExpressions{
+public class Controller implements RegularExpressions {
     private Model model;
     private View view;
 
-    public Controller(Model model,View view){
+    public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
     }
@@ -18,13 +18,13 @@ public class Controller implements RegularExpressions{
      * Запускает процес ввода необходимых значений, проверяет их, формирует строку(передает управление в модель)
      * и отображает на экран(передает управление на View)
      */
-    public void process(){
+    public void process() {
         Scanner scanner = new Scanner(System.in);
         ValueInputer valueInputer = new ValueInputer(scanner, view);
 
-        model.setFirstName(valueInputer.inputValueWhithScanner(firstName,View.INPUT_FIRST_NAME));
+        model.setFirstName(valueInputer.inputValueWhithScanner(firstName, View.INPUT_FIRST_NAME));
         model.setSecondName(valueInputer.inputValueWhithScanner(secondName, View.INPUT_SECOND_NAME));
-        model.setNickName(valueInputer.inputValueWhithScanner(nickName,View.INPUT_NICKNAME));
+        model.setNickName(valueInputer.inputValueWhithScanner(nickName, View.INPUT_NICKNAME));
         view.printMessage(View.NAME_VALUE + model.getFirstName() + View.ENDL +
                 View.SECOND_NAME_VALUE + model.getSecondName() + View.ENDL +
                 View.NICKNAME + model.getNickName());
